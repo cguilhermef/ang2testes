533f24d42d7d7b5897084cc2510fb0faf1e9ee27
Christian Guilherme Fritsch <guilherme@opiniaoearte.com.br>
2017-03-12 16:57:08 -0300

    Finalizada a parte 7 do tutorial.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,14 +14,15 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { HeroSearchComponent } from './hero-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     AppRoutingModule,
